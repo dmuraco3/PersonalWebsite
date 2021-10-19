fbdd0c225551ccf287c90c74bb9b522c8f4d57c5
dmuraco3 <70153277+dmuraco3@users.noreply.github.com>
2021-10-19 10:54:18 -0400

    boom

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,134 +1,115 @@
-import { useState, useEffect } from 'react';
-import Navigation from '../../components/navbar'
-import styles from './projects.module.scss';
+import { useState, useEffect } from "react";
+import Navigation from "../../components/navbar";
+import styles from "./projects.module.scss";
 
-import Image from 'next/image'
+import Image from "next/image";
 
-import Footer from 'components/Footer';
+import Footer from "components/Footer";
 
 export default function Projects() {
-    const [projects, setProjects] = useState();
-    const [evenProjects, setEvenProjects] = useState([]);
-    const [oddProjects, setOddProjects] = useState([]);
+  const [projects, setProjects] = useState();
+  const [evenProjects, setEvenProjects] = useState([]);
+  const [oddProjects, setOddProjects] = useState([]);
 
-    const projectsParser = (projects) => {
-        projects.forEach((project, index) => {
-            if(index % 2 === 0){
-                setEvenProjects(evenProjects => [...evenProjects, project])
-            } else if(index % 2 !== 0) {
-                setOddProjects(oddProjects => [...oddProjects, project])
-            }
+  const projectsParser = (projects) => {
+    projects.forEach((project, index) => {
+      if (index % 2 === 0) {
+        setEvenProjects((evenProjects) => [...evenProjects, project]);
+      } else if (index % 2 !== 0) {
+        setOddProjects((oddProjects) => [...oddProjects, project]);
+      }
+    });
+  };
+  useEffect(() => {
+    if (!projects) {
+      fetch(`${process.env.NEXT_PUBLIC_URL}/api/projects`)
+        .then((res) => res.json())
+        .then((data) => {
+          setProjects(data);
+          projectsParser(data);
+          console.log(data);
         })
+        .catch((err) => console.log(err));
     }
-    useEffect(() => {
-        if(!projects) {
-            fetch(`${process.env.NEXT_PUBLIC_URL}/api/projects`)
-                .then(res => res.json())
-                .then(data => {
-                    setProjects(data);
-                    projectsParser(data);
-                    console.log(data)
-                })
-                .catch(err => console.log(err))
+  });
 
-        }
-    })
+  const ProjectCard = ({ project, index }) => {
     return (
-        <main>
-            <Navigation />
-            <div className={styles.ProjectsContainer}>
-                <div className={styles.MobileProjects}>
-                    {projects && projects.map((project, index) => (
-                        <div className={styles.ProjectContainer} key={index}>
-                            <div className={styles.ProjectCard} >
-                                <div className={styles.CardHeader}>
-                                    <div className={styles.ProjectTitleContainer}>
-                                        <h1 className={styles.ProjectTitle}>{project.title}</h1>
-                                    </div>
-                                    <div className={styles.ProjectDateContainer}>
-                                        <span className={styles.ProjectDate}>{new Date(project.createdAt).toLocaleDateString()}</span>
-                                    </div>
-                                </div>
-                                <div className={styles.CardBody}>
-                                    <div className={styles.ProjectDescriptionContainer}>
-                                        <p className={styles.ProjectDescription}>{project.description}</p>
-                                    </div>
-                                    <div className={styles.ProjectImageContainer}>
-                                        <Image className={styles.ProjectImage} src={project.image} width="100%" alt={project.title} />
-                                    </div>
-                                    <div className={styles.ProjectLinksContainer}>
-                                        <a className={styles.WebsiteLink} target="_blank" rel="noreferrer" href={project.link}>Visit Website</a>
-                                        <a className={styles.GithubLink} target="_blank" rel="noreferrer" href={project.codeLink}>View on Github</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    ))}
-                </div>
-                <div className={styles.DesktopProjects}>
-
-                    <div className={styles.Left}>
-                        {evenProjects && evenProjects.map((project, index) => (
-                            <div className={styles.ProjectContainer} key={index}>
-                                <div className={styles.ProjectCard} >
-                                    <div className={styles.CardHeader}>
-                                        <div className={styles.ProjectTitleContainer}>
-                                            <h1 className={styles.ProjectTitle}>{project.title}</h1>
-                                        </div>
-                                        <div className={styles.ProjectDateContainer}>
-                                            <span className={styles.ProjectDate}>{new Date(project.createdAt).toLocaleDateString()}</span>
-                                        </div>
-                                    </div>
-                                    <div className={styles.CardBody}>
-                                        <div className={styles.ProjectDescriptionContainer}>
-                                            <p className={styles.ProjectDescription}>{project.description}</p>
-                                        </div>
-                                        <div className={styles.ProjectImageContainer}>
-                                            <Image className={styles.ProjectImage} src={project.image} width="100%" alt={project.title} />
-                                        </div>
-                                        <div className={styles.ProjectLinksContainer}>
-                                            <a className={styles.WebsiteLink} target="_blank" rel="noreferrer" href={project.link}>Visit Website</a>
-                                            <a className={styles.GithubLink} target="_blank" rel="noreferrer" href={project.codeLink}>View on Github</a>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        ))}
-                    </div>
-                    <div className={styles.Right}>
-                        {oddProjects && oddProjects.map((project, index) => (
-                            <div className={styles.ProjectContainer} key={index}>
-                                <div className={styles.ProjectCard} >
-                                    <div className={styles.CardHeader}>
-                                        <div className={styles.ProjectTitleContainer}>
-                                            <h1 className={styles.ProjectTitle}>{project.title}</h1>
-                                        </div>
-                                        <div className={styles.ProjectDateContainer}>
-                                            <span className={styles.ProjectDate}>{new Date(project.createdAt).toLocaleDateString()}</span>
-                                        </div>
-                                    </div>
-                                    <div className={styles.CardBody}>
-                                        <div className={styles.ProjectDescriptionContainer}>
-                                            <p className={styles.ProjectDescription}>{project.description}</p>
-                                        </div>
-                                        <div className={styles.ProjectImageContainer}>
-                                            <Image className={styles.ProjectImage} src={project.image} width="100%" alt={project.title} />
-                                        </div>
-                                        <div className={styles.ProjectLinksContainer}>
-                                            <a className={styles.WebsiteLink} target="_blank" rel="noreferrer" href={project.link}>Visit Website</a>
-                                            <a className={styles.GithubLink} target="_blank" rel="noreferrer" href={project.codeLink}>View on Github</a>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        ))}
-                    </div>
-                </div>
+      <div className={styles.ProjectContainer} key={index}>
+        <div className={styles.ProjectCard}>
+          <div className={styles.CardHeader}>
+            <div className={styles.ProjectTitleContainer}>
+              <h1 className={styles.ProjectTitle}>{project.title}</h1>
             </div>
-            <Footer />
-        </main>
-    )
+            <div className={styles.ProjectDateContainer}>
+              <span className={styles.ProjectDate}>
+                {new Date(project.createdAt).toLocaleDateString()}
+              </span>
+            </div>
+          </div>
+          <div className={styles.CardBody}>
+            <div className={styles.ProjectDescriptionContainer}>
+              <p className={styles.ProjectDescription}>{project.description}</p>
+            </div>
+            <div className={styles.ProjectImageContainer}>
+              <Image
+                className={styles.ProjectImage}
+                src={project.image}
+                layout="fill"
+                alt={project.title}
+                objectFit="cover"
+              />
+            </div>
+            <div className={styles.ProjectLinksContainer}>
+              <a
+                className={styles.WebsiteLink}
+                target="_blank"
+                rel="noreferrer"
+                href={project.link}
+              >
+                Visit Website
+              </a>
+              <a
+                className={styles.GithubLink}
+                target="_blank"
+                rel="noreferrer"
+                href={project.codeLink}
+              >
+                View on Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <main>
+      <Navigation />
+      <div className={styles.ProjectsContainer}>
+        <div className={styles.MobileProjects}>
+          {projects &&
+            projects.map((project, index) => (
+              <ProjectCard project={project} index={index} />
+            ))}
+        </div>
+        <div className={styles.DesktopProjects}>
+          <div className={styles.Left}>
+            {evenProjects &&
+              evenProjects.map((project, index) => (
+                <ProjectCard project={project} index={index} />
+              ))}
+          </div>
+          <div className={styles.Right}>
+            {oddProjects &&
+              oddProjects.map((project, index) => (
+                <ProjectCard project={project} index={index} />
+              ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 }

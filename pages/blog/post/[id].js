a7fd62898235ec915a4041b6c8b8ff593dc2a0fc
dmuraco3 <dylanjmuraco@gmail.com>
2021-10-23 07:45:29 -0400

    added blog post metadata

--- pages/blog/post/[id].js
+++ pages/blog/post/[id].js
@@ -1,10 +1,30 @@
 import styles from "./post.module.scss";
 import { PrismaClient } from "@prisma/client";
 import ReactHtmlParser from "react-html-parser";
+import Header from "next/head"
 
 function Blog({ post }) {
   return (
+
     <main>
+      <Header>
+        <title>{post.title}</title>
+        <meta name="description" content={post.description}/>
+
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:site" content="@MuracoDylan"/>
+        <meta name="twitter:title" content={post.description}/>
+        <meta name="twitter:description" content={post.description} />
+        <meta name="twitter:image" content="https://personal-website-phi-six.vercel.app/ProfilePig.jpg"/>
+
+
+        <meta property="og:title" content={post.title}/>
+        <meta property="og:description" content={post.description}/>
+        <meta property="og:image" content="https://personal-website-phi-six.vercel.app/ProfilePig.jpg"/>
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_URL}/blog/post/${post.id}`}/>
+
+        <meta name="keywords" content="web development, coding, javascript, react"/>
+      </Header>
       <div className={styles.mainContainer}>
         <div className={styles.PostHeading}>
           <h1>{post.title}</h1>

579d0c888ea8a70d9c2487d06f31fea493314319
dmuraco3 <70153277+dmuraco3@users.noreply.github.com>
2021-10-25 12:52:40 -0400

    now server side rendering home page

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,24 @@
 import { useRouter } from "next/router";
-import ProjectStyles from './homeProjects.module.scss'
+import ProjectStyles from "./homeProjects.module.scss";
 
 import Typist from "react-typist";
 import { FaChevronDown as Down, FaChevronRight as Right } from "react-icons/fa";
 
-import Image from 'next/image'
-import Header from 'next/head'
+import Image from "next/image";
+import Header from "next/head";
 
 import Spinner from "react-bootstrap/Spinner";
 import Card from "react-bootstrap/Card";
 
-import { useEffect, useState, useRef } from "react";
+import { useRef } from "react";
 
-export default function Home() {
-  const [posts, setPosts] = useState(null);
-  const [projects, setProjects] = useState(null);
+export default function Home({ projects, posts }) {
   const hostUrl = process.env.NEXT_PUBLIC_URL;
   const portfolio = useRef(null);
 
   const executeScroll = () => {
     portfolio.current.scrollIntoView();
   };
-
-  useEffect(() => {
-    if (!projects) {
-      const url = `${process.env.NEXT_PUBLIC_URL}/api/projects`;
-      fetch(url)
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          setProjects(json);
-        });
-    }
-    if (!posts) {
-      fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog/posts`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((json) => {
-          setPosts(json);
-        });
-    }
-  }, [posts, setPosts, projects, setProjects]);
 
   function ActiveLink({ children, href }) {
     const router = useRouter();
@@ -68,7 +43,10 @@ export default function Home() {
     <main>
       <Header>
         <title>Dylan Muraco</title>
-        <meta name="description" content="My name is Dylan Muraco. I've been desiging and building applications for 3 years."/>
+        <meta
+          name="description"
+          content="My name is Dylan Muraco. I've been desiging and building applications for 3 years."
+        />
       </Header>
       <div className="focal">
         <div className="focal-background"></div>
@@ -114,21 +92,36 @@ export default function Home() {
                     <div className={ProjectStyles.ProjectDateContainer}>
                       <span className={ProjectStyles.ProjectDate}>
                         {new Date(item.createdAt).toLocaleDateString()}
-                        </span>
+                      </span>
                     </div>
                     <div className={ProjectStyles.ProjectTitleContainer}>
-                      <h1 className={ProjectStyles.ProjectTitle}>{item.title}</h1>
+                      <h1 className={ProjectStyles.ProjectTitle}>
+                        {item.title}
+                      </h1>
                     </div>
                     <div className={ProjectStyles.ProjectImageContainer}>
-                      <Image className={ProjectStyles.ProjectImage} src={item.image} layout="fill" objectFit="cover"/>
+                      <Image
+                        className={ProjectStyles.ProjectImage}
+                        src={item.image}
+                        layout="fill"
+                        objectFit="cover"
+                      />
                     </div>
                     <div className={ProjectStyles.ProjectDescriptionContainer}>
-                      <p className={ProjectStyles.ProjectDescription}>{item.description}</p>
+                      <p className={ProjectStyles.ProjectDescription}>
+                        {item.description}
+                      </p>
                     </div>
                     <div className={ProjectStyles.ProjectLinkContainer}>
-                      <a className={ProjectStyles.ProjectLink} href={item.link}>View Website</a>
-                      <a className={ProjectStyles.CodeLink} href={item.codeLink}>View Code</a>
-
+                      <a className={ProjectStyles.ProjectLink} href={item.link}>
+                        View Website
+                      </a>
+                      <a
+                        className={ProjectStyles.CodeLink}
+                        href={item.codeLink}
+                      >
+                        View Code
+                      </a>
                     </div>
                   </div>
                 ))}
@@ -152,23 +145,24 @@ export default function Home() {
           </center>
         ) : (
           <div className="blog-posts" style={{ borderRadius: "6px" }}>
-            {posts.map((item, index) => (
-              <Card key={index} style={{ width: "100%" }}>
-                <Card.Body>
-                  <Card.Title>
-                    <ActiveLink href={`${hostUrl}/blog/post/${item.id}`}>
-                      {item.title}
-                    </ActiveLink>
-                  </Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {item.date}
-                  </Card.Subtitle>
-                  <Card.Text style={{ width: "50%" }}>
-                    {item.description}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            ))}
+            {posts &&
+              posts.map((item, index) => (
+                <Card key={index} style={{ width: "100%" }}>
+                  <Card.Body>
+                    <Card.Title>
+                      <ActiveLink href={`${hostUrl}/blog/post/${item.id}`}>
+                        {item.title}
+                      </ActiveLink>
+                    </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {item.date}
+                    </Card.Subtitle>
+                    <Card.Text style={{ width: "50%" }}>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
           </div>
         )}
       </div>
@@ -176,11 +170,27 @@ export default function Home() {
         <h1>Let's Work Together</h1>
         <h4>Have a project you'd like to discuss?</h4>
         <div className="touch-me-container">
-          <a className="touch-me" href={`${process.env.NEXT_PUBLIC_URL}/contact`}>
+          <a
+            className="touch-me"
+            href={`${process.env.NEXT_PUBLIC_URL}/contact`}
+          >
             Get in Touch
           </a>
         </div>
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps(context) {
+  const projectRes = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/projects`);
+  const postRes = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog/posts`);
+  const projects = await projectRes.json();
+  const posts = await postRes.json();
+  return {
+    props: {
+      projects,
+      posts
+    }
+  };
 }
